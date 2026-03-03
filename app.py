761882f838b22a80a82ff82a14689ce842235e91
tejas49167-ui <tejas49167@gmail.com>
2026-03-03 16:08:50 +0530

    changed something

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ API_KEY = os.getenv("GROQ_API_KEY")
 def home():
     return render_template("index.html")
 
-@app.route("/chat", methods=["POST"])
+
 @app.route("/chat", methods=["POST"])
 def chat():
     user_message = request.json["message"].lower()
@@ -55,4 +55,5 @@ def chat():
 
     return jsonify({"reply": reply})
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
